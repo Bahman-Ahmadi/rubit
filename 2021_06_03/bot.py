b4441be0c9b264cbc5175974ba0e05318f1794e7
Bahman-Ahmadi <bahmanahmadi.mail@gmail.com>
2021-08-05 19:02:50 +0430

    i tried for fixing replying problem

--- 2021_06_03/bot.py
+++ 2021_06_03/bot.py
@@ -76,7 +76,7 @@ while 1:
 		elif not i.text in conditions and i.text != "else":
 			exec(codes[conditions.index("else")])
 		
-		opt.send("TEXT= "+i.text,1)
+		opt.send("TEXT= "+str(i.text),True)
 	print("message loop ended")
 	log("OK",57,"ChatRoom’s messages executed!")
 
